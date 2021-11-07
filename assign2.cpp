602684fa6341e5b16cab0e1c97acf278351b393f
anina <kucingpeci2lan@gmail.com>
2021-11-07 22:13:22 +0700

    learning related to printf
    print some variables (int)

---
+++ assign2.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int x;
+int y;
+
+int main(){
+	x = 99;
+	y = 100;
+	
+	printf("Nilai x = %d\n", x);
+	printf("Nilai y = %d\n", y);
+	
+	x = 262020;
+	printf("Nilai x sekarang = %d\n", x);
+	
+}
