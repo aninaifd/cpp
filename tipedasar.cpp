c56ddfce9882ff889acb265085d643fd3c9c3b86
anina <kucingpeci2lan@gmail.com>
2021-11-07 22:19:05 +0700

    basic data type
    integer and double

---
+++ tipedasar.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int number1, number2;
+double numb1, numb2;
+
+int main(){
+	number1 = 27;
+	number2 = number1;
+	printf("%d, %d\n", number1, number2);
+	
+	numb1 = 3.14159;
+	numb2 = 891.3757;
+	printf("%lf\n", numb1);
+	printf("%lf\n", numb2);
+}
